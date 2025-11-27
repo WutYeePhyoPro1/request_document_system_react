e9cde2f859424073c638e1be9ee88daffd7864e0
HninSuWai12 <hninsuwai180@gmail.com>
2025-11-27 11:18:53 +0630

    hninsuwai update

--- src/pages/requestDiscount/index.tsx
+++ src/pages/requestDiscount/index.tsx
@@ -371,7 +371,7 @@ export default function Demo() {
                   <Table.Tr>
           <Table.Td colSpan={7}>
             <div className="flex flex-col items-center justify-center py-10">
-              <Loader size="xl" color="blue" />
+              {/* <Loader size="xl" color="blue" /> */}
               <p className="mt-4 text-lg font-semibold text-gray-700 animate-pulse">
                 Loading Data.Please wait!...
               </p>
