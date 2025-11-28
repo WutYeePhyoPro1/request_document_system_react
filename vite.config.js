28ce8a04d39b783eeebdf53bc492d5d85a97830e
HninSuWai12 <hninsuwai180@gmail.com>
2025-11-28 14:43:39 +0630

    check performance

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from "vite-tsconfig-paths"
 
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react(), tailwindcss() , tsconfigPaths()],
+  return {
+    plugins: [react(), tailwindcss(), tsconfigPaths()],
     server: {
-        host: true,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8000',
-                // target: 'https://rds.sdpghc.net/',
-                changeOrigin: true,
-                secure: false,
-                rewrite: (path) => path.replace(/^\/api/, '/api')
-            }
+      host: true,
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (p) => p.replace(/^\/api/, '/api')
         }
+      }
     }
-});
+  }
+})
