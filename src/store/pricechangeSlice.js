367225c330a506cbaa6b5ea9db64a287efe6fbdb
Thoon Thoon <thoonlay779@gmail.com>
2026-03-23 17:07:56 +0630

    columns toogle

--- src/store/pricechangeSlice.js
+++ src/store/pricechangeSlice.js
@@ -5,6 +5,30 @@ import {formatDate,formatStrDateTime,formatTo2Decimals,formatLaravelStyleDate} f
 
 const API_URL =  `https://dummyjson.com/products?limit=`;
 
+const STORAGE_KEY = "pricechange_visible_columns";
+const DEFAULT_COLUMNS = [
+  { slug: "actions", name: "Actions", visible: true, requireAuth: true, display: true },
+  { slug: "no", name: "No", visible: true, display: true  },
+  { slug: "product_code", name: "Product Code", visible: true , display: true },
+  { slug: "product_name", name: "Product Name", visible: true },
+  { slug: "unit", name: "Unit", visible: true },
+  { slug: "price", name: "Ref Price", visible: true, numeric: true },
+  { slug: "new_cost_price", name: "New Cost Price", visible: true, numeric: true },
+  { slug: "price1", name: "Price 1", visible: true, numeric: true },
+  { slug: "price2", name: "Price 2", visible: true, numeric: true },
+  { slug: "profit", name: "Profit", visible: true, numeric: true },
+];
+
+
+const loadVisible = () => {
+  try {
+    const saved = localStorage.getItem(STORAGE_KEY);
+    return saved ? JSON.parse(saved) : DEFAULT_COLUMNS.filter(c => c.visible).map(c => c.slug);
+  } catch {
+    return DEFAULT_COLUMNS.filter(c => c.visible).map(c => c.slug);
+  }
+};
+
 export const fetchPriceChanges = createAsyncThunk( "property/fetchPriceChange", async({filters,page=1,searchQuery=''}={},{})=>{
      const token = localStorage.getItem('token');
 
@@ -38,26 +62,49 @@ const picechangeSlice = createSlice({
                branch_id: "",
                category_id: ""
           },
+          columns: DEFAULT_COLUMNS,
+          visibleColumns: loadVisible(),
           isSearchMode: false,
           paginationInfo: null
      },
      reducers: {
-         setFilter(state,action){
-               state.filters = {...state.filters,...action.payload};
-               state.isSearchMode = true;
-         },
-         clearFilters(state){
-               state.filters = {
-                    form_doc_no: "",
-                    start_date: "",
-                    end_date: "",
-                    search_status: [],
-                    branch_id: "",
-                    category_id: ""
-               };
-               state.isSearchMode = false;
-               state.paginationInfo = null;
-         },
+          setFilter(state,action){
+                    state.filters = {...state.filters,...action.payload};
+                    state.isSearchMode = true;
+          },
+          clearFilters(state){
+                    state.filters = {
+                         form_doc_no: "",
+                         start_date: "",
+                         end_date: "",
+                         search_status: [],
+                         branch_id: "",
+                         category_id: ""
+                    };
+                    state.isSearchMode = false;
+                    state.paginationInfo = null;
+          },
+          toggleColumn(state, action) {
+               const slug = action.payload;
+               if (state.visibleColumns.includes(slug)) {
+                    state.visibleColumns = state.visibleColumns.filter(c => c !== slug);
+               } else {
+                    state.visibleColumns.push(slug);
+               }
+
+               localStorage.setItem(STORAGE_KEY, JSON.stringify(state.visibleColumns));
+          },
+
+          setAllColumns(state, action) {
+               state.visibleColumns = action.payload;
+               localStorage.setItem(STORAGE_KEY, JSON.stringify(state.visibleColumns));
+          },
+
+          clearColumns(state) {
+               // state.visibleColumns = [];
+               state.visibleColumns = DEFAULT_COLUMNS.filter(c => c.display).map(c => c.slug)
+               localStorage.setItem(STORAGE_KEY, JSON.stringify([]));
+          }
      },
      extraReducers: (builder)=>{
 
@@ -73,7 +120,7 @@ const picechangeSlice = createSlice({
                     // state.datas = result;
                     state.datas = result.map(item => ({
                         ...item,
-                        created_at: formatLaravelStyleDate(item.created_at,'Y-m-d'),
+                        created_at: formatLaravelStyleDate(item.created_at,'Y-m-d H:i:s'),
                         date: formatLaravelStyleDate(item.date_formatted,'Y-m-d'),
                     }))
                     console.log(action.payload.data.data)
@@ -88,7 +135,7 @@ const picechangeSlice = createSlice({
      }
 });
 
-export const {setFilter,clearFilters,isFiltersEmpty} = picechangeSlice.actions;
+export const {setFilter,clearFilters,isFiltersEmpty, toggleColumn, setAllColumns, clearColumns} = picechangeSlice.actions;
 export default picechangeSlice.reducer;
 
 // 
